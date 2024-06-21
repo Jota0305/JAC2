895a0d53fbb4cb4397c2f6c6bbbef6fbc11835ef
Jose Tasilla <jozeh@hotmail.es>
2024-06-21 16:44:58 -0500

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import React from "react";
-import Header from "./pages/Header.jsx";
+import Home from "./pages/Home.jsx"
+import About from "./pages/About.jsx"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
 
   return (
-    <Header/>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/" element={<About/>} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
