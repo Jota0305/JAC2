ea6cd39f0df1ae7bad608e8513c7ed1ec8d74efc
Jose Tasilla <jozeh@hotmail.es>
2024-06-22 21:27:21 -0500

    init slaider

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import Header from '../components/Header';
+import Slaider from '../components/Slider/Slider.jsx';
 
 function Home() {
   return (
-    <Header/>
+    <div>
+      <Header/>
+      <Slaider/>
+    </div>
   )
 }
 
