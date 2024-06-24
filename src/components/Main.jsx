d93621bbae9a63d8f6a625bad4ba6db8be0bfeb2
AnderH4ck <anderson2020tasilla@gmail.com>
2024-06-23 22:44:23 -0500

    Main listo

---
+++ src/components/Main.jsx
@@ -0,0 +1,35 @@
+function Main() {
+  return (
+    <div className="bg-purple-100 w-full h-80">
+      <div className="px-44 py-11">
+        <div className="flex items-center gap-10">
+          <div className="text-justify p-5">
+            <div className="relative text-purple-700 font-bold">
+              <span className="absolute left-0 bottom-[10px] w-10 border-b-[1px] border-pink-500"></span>
+              <h4 className="ml-12 font-bold text-base">About Us</h4>
+            </div>
+            <h1 className="mb-6 text-4xl font-bold">Bienvendio a Dentile</h1>
+            <p className="text-gray-700">
+              Ea consequuntur illum facere aperiam sequi optio consectetur
+              adipisicing elitFuga, suscipit totam animi consequatur saepe
+              blanditiis.Lorem ipsum dolor sit amet,Ea consequuntur illum facere
+              aperiam sequi optio consectetur adipisicing elit. Fuga, suscipit
+              totam animi consequatur saepe blanditiis.
+            </p>
+          </div>
+          <div className="text-justify p-5 mt-[86px]">
+            <p className="text-gray-700">
+              Ea consequuntur illum facere aperiam sequi optio consectetur
+              adipisicing elitFuga, suscipit totam animi consequatur saepe
+              blanditiis.Lorem ipsum dolor sit amet,Ea consequuntur illum facere
+              aperiam sequi optio consectetur adipisicing elit. Fuga, suscipit
+              totam animi consequatur saepe blanditiis.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Main;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,16 @@
-import React from 'react'
-import Header from '../components/Header';
-import Slaider from '../components/Slider/Slider.jsx';
+import React from "react";
+import Header from "../components/Header";
+import Slaider from "../components/Slider/Slider.jsx";
+import Main from "../components/Main.jsx";
 
 function Home() {
   return (
     <div>
-      <Header/>
-      <Slaider/>
+      <Header />
+      <Slaider />
+      <Main />
     </div>
-  )
+  );
 }
 
 export default Home;
