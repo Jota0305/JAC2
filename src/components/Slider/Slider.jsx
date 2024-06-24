90b6cb0df4a9ac803cbf86ef9524133d65d51dc6
Jose Tasilla <jozeh@hotmail.es>
2024-06-24 00:22:38 -0500

    update slaider

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -1,24 +1,56 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import Slider from 'react-slick';
+import { RiArrowRightSLine, RiArrowLeftSLine  } from "react-icons/ri";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 const ImageSlider = () => {
+  const sliderRef = useRef(null); // Referencia al slider para controlar la navegación
 
   const images = [
     'src/assets/slaider1.jpg',
     'src/assets/slaider2.jpg',
     'src/assets/slaider3.jpg',
     'src/assets/slaider4.jpg',
-
   ];
 
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
+
+  const adelante = () => {
+    sliderRef.current.slickNext(); // Avanzar al siguiente slide
+  };
+
+  const atras = () => {
+    sliderRef.current.slickPrev(); // Retroceder al slide anterior
+  };
+
   return (
-    <div className="w-full overflow-hidden">
-      <Slider>
+    <div className="w-full overflow-hidden relative">
+      <Slider {...settings} ref={sliderRef}>
         {images.map((image, index) => (
           <div key={index}>
-            <img src={image} alt={`Slide ${index}`} className="w-full h-[590px] object-cover" />
+            <div className="relative bg-black">
+              <img src={image} alt={`Slide ${index}`} className="opacity-50 w-full h-[590px] object-cover" />
+
+              <button
+                className="absolute top-1/2 left-4 transform -translate-y-1/2 bg-gray-800 text-white px-4 py-4 rounded-md hover:opacity-50"
+                onClick={adelante}
+              >
+                <RiArrowLeftSLine />
+              </button>
+              <button
+                className="absolute top-1/2 right-4 transform -translate-y-1/2 bg-gray-800 text-white px-4 py-4 rounded-md hover:opacity-50"
+                onClick={atras}
+              >
+               <RiArrowRightSLine />
+              </button>
+            </div>
           </div>
         ))}
       </Slider>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,20 +4,12 @@ import ImageSlider from "../components/Slider/Slider.jsx";
 import Main from "../components/Main.jsx";
 
 function Home() {
-  // const listaImagenes = [
-  //   "src/assets/slaider1.jpg",
-  //   "src/assets/slaider2.jpg",
-  //   "src/assets/slaider3.jpg",
-  //   "src/assets/slaider4.jpg",
-  // ];
-
 
   return (
     <div>
       <Header />
       <ImageSlider/>
       <Main />
-      {/* <Slaider imagenes={listaImagenes} /> */}
     </div>
   );
 
