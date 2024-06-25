7394ae7e636ee4d5b6c708be3e9d226171a71b10
AnderH4ck <anderson2020tasilla@gmail.com>
2024-06-24 23:36:47 -0500

    Slaider Acabado

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -8,10 +8,38 @@ const ImageSlider = () => {
   const sliderRef = useRef(null); // Referencia al slider para controlar la navegación
 
   const images = [
-    "src/assets/slaider1.jpg",
-    "src/assets/slaider2.jpg",
-    "src/assets/slaider3.jpg",
-    "src/assets/slaider4.jpg",
+    {
+      src: "src/assets/slaider1.jpg",
+      texts: [
+        { text: "Dental Care For Life", className: "text-lg font-bold" },
+        { text: "Experience, Trust And", className: "text-6xl font-bold" },
+        { text: "Proven Success", className: "text-6xl font-bold" },
+      ],
+    },
+    {
+      src: "src/assets/slaider3.jpg",
+      texts: [
+        { text: "Care For Your Smile", className: "text-lg font-bold" },
+        { text: "Get All The Essential", className: "text-6xl font-bold" },
+        { text: "Dental Care Today", className: "text-6xl font-bold" },
+      ],
+    },
+    {
+      src: "src/assets/slaider2.jpg",
+      texts: [
+        { text: "Dental Care For Life", className: "text-lg font-bold" },
+        { text: "Experience, Trust And", className: "text-6xl font-bold" },
+        { text: "Proven Success", className: "text-6xl font-bold" },
+      ],
+    },
+    {
+      src: "src/assets/slaider4.jpg",
+      texts: [
+        { text: "Care For Your Smile", className: "text-lg font-bold" },
+        { text: "Get All The Essential", className: "text-6xl font-bold" },
+        { text: "Dental Care Today", className: "text-6xl font-bold" },
+      ],
+    },
   ];
 
   const settings = {
@@ -37,11 +65,23 @@ const ImageSlider = () => {
           <div key={index}>
             <div className="relative bg-black">
               <img
-                src={image}
+                src={image.src}
                 alt={`Slide ${index}`}
                 className="opacity-50 w-full h-[590px] object-cover"
               />
-
+              <div className="absolute inset-0 flex flex-col justify-center items-center z-1 text-white">
+                {image.texts.map((textItem, textIndex) => (
+                  <div key={textIndex} className={` ${textItem.className}`}>
+                    {textItem.text}
+                  </div>
+                ))}
+                <a
+                  href="#"
+                  className="bg-rose-500 font-bold transition ease-in-out duration-600 hover:bg-violet-900 text-white py-3 px-5 rounded-lg mt-11"
+                >
+                  Ver Nuestro Servicios
+                </a>
+              </div>
               <button
                 className="absolute top-1/2 left-4 transform -translate-y-1/2 bg-gray-800 text-white px-4 py-4 rounded-md hover:opacity-50"
                 onClick={atras}
